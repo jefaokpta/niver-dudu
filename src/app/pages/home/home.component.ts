870046cf09197ccfb0f96bc7bb315d1e7ba9330c
jefaokpta <jefaokpta@hotmail.com>
2022-06-25 14:44:24 -0300

    validadores ok

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
+  form = this.formBuilder.group({
+    name: ['', Validators.required],
+    phone: ['', [Validators.required]],
+  })
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) { }
+
+  confirm() {
+    console.log(this.form.valid);
+    console.log(this.form.value);
+  }
+
+  validOrTouched(fieldName: string): boolean{
+    const field = this.form.get(fieldName)
+    return field!!.touched && !field!!.valid
   }
 
 }
