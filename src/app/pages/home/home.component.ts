5ff23c855277005ae501ba549af7100b900f1154
jefaokpta <jefaokpta@hotmail.com>
2022-06-26 18:39:33 -0300

    todas as funcoes ok

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,21 +8,43 @@ import {FormArray, FormBuilder, Validators} from "@angular/forms";
 })
 export class HomeComponent {
 
+  sendButtornLoading: boolean = true;
   form = this.formBuilder.group({
-    name: ['', Validators.required],
-    phone: ['', [Validators.required]],
+    name: [null, Validators.required],
+    phone: [null, [Validators.required]],
+    guests: this.formBuilder.array([])
   })
 
-  constructor(private formBuilder: FormBuilder,
-              ) { }
+  constructor(private formBuilder: FormBuilder) { }
+
+  get guests() {
+    return this.form.get('guests') as FormArray;
+  }
+
+  addGuest() {
+    this.guests.push(this.formBuilder.group({
+      guestName: [null, Validators.required],
+      guestAge: [null, Validators.required],
+    }));
+  }
+
+  removeGuest(index: number) {
+    this.guests.removeAt(index);
+  }
 
   confirm() {
+    this.sendButtornLoading = false;
     console.log(this.form.valid);
     console.log(this.form.value);
   }
 
   validOrTouched(fieldName: string): boolean{
     const field = this.form.get(fieldName)
+    return field!!.touched && !field!!.valid
+  }
+
+  validOrTouchedGuests(fieldName: string, index: number): boolean{
+    const field = this.form.get('guests')?.get(index.toString())?.get(fieldName)
     return field!!.touched && !field!!.valid
   }
 
