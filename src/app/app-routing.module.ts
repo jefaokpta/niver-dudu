e8fdb60d6452ba7b82133e47688e69b722451362
jefaokpta <jefaokpta@hotmail.com>
2022-07-01 18:47:22 -0300

    admin fazendo oq precisa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }];
+const routes: Routes = [
+  {path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
+  {path: 'pages/admin', loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule)},
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/admin/admin.component.ts
@@ -0,0 +1,81 @@
+import { Component } from '@angular/core';
+import {FormArray, FormBuilder, Validators} from "@angular/forms";
+import {ActivatedRoute} from "@angular/router";
+import {ServerService} from "../../services/server.service";
+import {Guest} from "../../model/Participant";
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.scss']
+})
+export class AdminComponent {
+
+  sendButtornLoading = true;
+  sendButtonText = 'Salvar';
+
+  private param = this.activatedRoute.snapshot.paramMap.get('idParticipant');
+
+  form = this.formBuilder.group({
+    name: [null, Validators.required],
+    phone: [null, [Validators.required]],
+    confirmed: [false],
+    guests: this.formBuilder.array([])
+  })
+
+  constructor(private formBuilder: FormBuilder,
+              private activatedRoute: ActivatedRoute,
+              private server: ServerService) {}
+
+  get guests() {
+    return this.form.get('guests') as FormArray;
+  }
+
+  addGuest(guest: Guest) {
+    this.guests.push(this.formBuilder.group({
+      id: [guest.id],
+      name: [guest.name, Validators.required],
+      isKid: [guest.isKid],
+    }));
+  }
+
+  removeGuest(index: number) {
+    this.guests.removeAt(index);
+  }
+
+  confirm() {
+    console.log(this.form.value);
+    if(this.form.valid){
+      this.sendButtornLoading = false;
+      this.server.create(this.form.value).subscribe({
+        next: (response) => {
+          this.form.reset()
+          console.log(response);
+        },
+        error: (error) => {
+          alert(error.message)
+          this.sendButtornLoading = true;
+          console.log('CAGOU', error);
+        },
+        complete: () => {
+          console.log('COMPLETO');
+          this.sendButtornLoading = true;
+        }
+      })
+    } else{
+      this.form.markAllAsTouched();
+      alert('Preencha todos os campos corretamente');
+    }
+  }
+
+  validOrTouched(fieldName: string): boolean{
+    const field = this.form.get(fieldName)
+    return field!!.touched && !field!!.valid
+  }
+
+  validOrTouchedGuests(fieldName: string, index: number): boolean{
+    const field = this.form.get('guests')?.get(index.toString())?.get(fieldName)
+    return field!!.touched && !field!!.valid
+  }
+
+}
